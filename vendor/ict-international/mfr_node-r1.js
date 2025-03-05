7ac050aa120999337177f97f30bb54bc298aa159
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-03-05 16:20:41 -0600

    added mfr_node ICT device

---
+++ vendor/ict-international/mfr_node-r1.js
@@ -0,0 +1,5 @@
+/*
+MFR nodes are data loggers so each decoder is different since each MFR node can have different sensors.
+ICT can provide you with your own decoder. When you have your own decoder, make sure it is compatible 
+with lorawan listener plugin. Visit the Sage plugin documentation for more information.
+*/
