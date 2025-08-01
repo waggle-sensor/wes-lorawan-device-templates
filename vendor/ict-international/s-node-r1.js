c02238234dc5fc7aed5d0f7058793d7b6211991f
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-08-01 10:59:46 -0500

    added new device for ICT

---
+++ vendor/ict-international/s-node-r1.js
@@ -0,0 +1,5 @@
+/*
+S nodes are data loggers so each decoder is different since each S node can have different sensors.
+ICT can provide you with your own decoder. When you have your own decoder, make sure it is compatible
+with lorawan listener plugin. Visit the Sage plugin documentation for more information.
+*/
